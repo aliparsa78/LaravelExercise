c7bbbd9fd1c8c3bd3485c7e1c83957d0de1009fe
aliparsa <aliparsa9@gmail.com>
2024-02-09 14:48:07 +0430

    Add middleware

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/',function(){
     return view('welcome');
 });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
+Route::group(['middleware'=>['auth','userCheck']],function(){
+    
+    Route::get('/dashboard',function(){
         return view('dashboard');
     })->name('dashboard');
+    
+    Route::get('/greeting', function () {
+        return 'Hello World';
+    });
 });
